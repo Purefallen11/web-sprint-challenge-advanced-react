0b37f345def888de4e56892024e50306fcc58233
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-10-10 20:44:46 -0500

    made axios request to backend, plants display

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -3,8 +3,21 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-  state = {
-    plants:[]
+  constructor(){
+    super();
+    this.state = {
+      plants:[]
+    }
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:3333/plants')
+    .then(res =>{
+      this.setState({
+        plants: res.data
+      })
+    })
+    .catch(err =>{console.log(err)})
   }
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
