ce1c032fbaaab12d24fd6ea61074a402165464fc
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-10-10 21:15:28 -0500

    changed state logic on checkout form to be handled by useform

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,19 @@
 // write your custom hook here to control your checkout form
+import {useState} from 'react'
+
+const useForm = (key,initialValues) =>{
+	const [storedValue, setStoredValue] = useState(() =>{
+		const item = window.localStorage.getItem(key);
+		return item ? JSON.parse(item) : initialValues;
+	});
+
+	const setValue = value =>{
+		setStoredValue(value)
+		window.localStorage.setItem(key, JSON.stringify(value))
+	}
+
+	return [storedValue, setValue]
+
+}
+
+export default useForm
