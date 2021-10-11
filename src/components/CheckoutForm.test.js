d94b05ddddab42417342ceb79f99c8fe8c2839c4
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-10-10 22:04:17 -0500

    writing test for checkoutform

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -12,6 +12,13 @@ test("renders without errors", () => {
 
 test("shows success message on submit with form details", () => {
 	render(<CheckoutForm />)
+
+	const fn = screen.getByLabelText(/first name/i)
+	const ln = screen.getByLabelText(/last name/i)
+	const addr = screen.getByLabelText(/address/i)
+	const city = screen.getByLabelText(/city/i)
+	const state = screen.getByLabelText(/state/i)
+	const zip = screen.getByLabelText(/zip/i)
 
 	
 });
